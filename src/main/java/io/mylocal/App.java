12e408dc1134a09da056db40e2487d667fe00a80
Anmol Mittal <anmol.mittal@jumbotail.com>
2024-06-30 00:45:58 +0530

    Adding support for multiple client using threads

--- src/main/java/io/mylocal/App.java
+++ src/main/java/io/mylocal/App.java
@@ -9,42 +9,63 @@ import java.net.Socket;
 import java.util.Locale;
 
 public class App {
+
+    private static void handleClient(Socket clientSocket) {
+        BufferedReader stdIn = null;
+        PrintWriter out = null;
+        try {
+
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.toLowerCase(Locale.ROOT).contains("ping"))
+                    out.println("PONG");
+            }
+        } catch (Exception e) {
+            System.out.println("Error while handling client request: " + e.getMessage());
+        } finally {
+            try {
+                if (stdIn != null) {
+                    stdIn.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (Exception e) {
+                System.out.println("Error while closing streams in handle client");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Logs From the program will appear here");
 
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
 
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
-            clientSocket = serverSocket.accept();
 
-            while(true) {
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                // Wait for connection from client.
+                Socket clientSocket = serverSocket.accept();
 
-                String line;
-                while((line = in.readLine()) != null)
-                {
-                    if(line.toLowerCase(Locale.ROOT).contains("ping"))
-                        out.println("PONG");
-                }
+                new Thread(() -> {
+                    try {
+                        handleClient(clientSocket);
+                    } catch (Exception e) {
+                        System.out.println("Error in main: " + e.getMessage());
+                    }
+                }).start();
             }
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
         }
     }
 }
